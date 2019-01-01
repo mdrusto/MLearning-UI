074abbf565744b8dfb0b5b178e303e45e04701ca
Matthew Drummond-Stoyles <yafta98@gmail.com>
2018-12-31 20:11:30 -0500

    Added digit file input

--- MLearning UI/App.xaml.cs
+++ MLearning UI/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
 
 namespace MLearning_UI
 {
@@ -13,5 +15,65 @@ namespace MLearning_UI
     /// </summary>
     public partial class App : Application
     {
+        public DigitImage[] images;
+
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            LoadImages();
+            int index = 301;
+            for (int x = 0; x < 28; x++)
+            {
+                for (int y = 0; y < 28; y++)
+                {
+                    ((MainWindow)this.MainWindow).setInputShade(y, x, images[index].GetValueAt(y, x));
+                }
+            }
+            ((MainWindow)this.MainWindow).setLabel(images[index].label);
+        }
+
+        public class DigitImage
+        {
+            public byte[,] contents;
+            public byte label;
+
+            public DigitImage(byte[,] contents, byte label)
+            {
+                this.contents = contents;
+                this.label = label;
+            }
+
+            public byte GetValueAt(int y, int x)
+            {
+                return contents[y,x];
+            }
+
+            public byte GetLabel()
+            {
+                return label;
+            }
+        }
+
+        public void LoadImages()
+        {
+            images = new DigitImage[60000];
+            byte[] imageFileContents = File.ReadAllBytes("digits/train-images.idx3-ubyte");
+            byte[] labelFileContents = File.ReadAllBytes("digits/train-labels.idx1-ubyte");
+            for (int counter = 0; counter < 60000; counter++)
+            {
+                
+                byte label = labelFileContents[counter + 8];
+
+                byte[,] imageContents = new byte[28, 28];
+                for (int pixelCounterX = 0; pixelCounterX < 28; pixelCounterX++)
+                {
+                    for (int pixelCounterY = 0; pixelCounterY < 28; pixelCounterY++)
+                    {
+                        imageContents[pixelCounterY, pixelCounterX] = imageFileContents[16 + 784 * counter + pixelCounterY * 28 + pixelCounterX];
+                    }
+                }
+                images[counter] = new DigitImage(imageContents, label);
+            }
+        }
     }
 }
--- MLearning UI/MainWindow.xaml.cs
+++ MLearning UI/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace MLearning_UI
         {
             InitializeComponent();
         }
+
+        public void setInputShade(int y, int x, byte value)
+        {
+            SolidColorBrush brush = new SolidColorBrush();
+            brush.Color = Color.FromRgb(value, value, value);
+            Rectangle rectangle = new Rectangle();
+            rectangle.Fill = brush;
+            Grid.SetRow(rectangle, y);
+            Grid.SetColumn(rectangle, x);
+            rectangle.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            rectangle.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            rectangle.Width = 10;
+            rectangle.Height = 10;
+            Panel.SetZIndex(rectangle, 0);
+            NetworkInputDisplay.Children.Add(rectangle);
+        }
+
+        public void setLabel(byte label)
+        {
+            SolidColorBrush brush = new SolidColorBrush();
+            brush.Color = Color.FromRgb(128, 128, 128);
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = brush;
+            Grid.SetRow(ellipse, label);
+            Grid.SetColumn(ellipse, 1);
+            ellipse.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            ellipse.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            ellipse.Width = 28;
+            ellipse.Height = 28;
+            NetworkOutputDisplay.Children.Add(ellipse);
+        }
     }
 }
